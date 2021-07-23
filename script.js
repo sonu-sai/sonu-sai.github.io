86588884ad052340b8036b680dd90aab18fdc6af
sonu-sai <sonusaist@gmail.com>
2021-07-23 10:13:06 +0530

    removed lightmode& darkmode buttons

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 function expand(idName) {
   document.querySelector("#" + idName).classList.add("reveal");
 }
-
-function dark(mode) {
-  document.querySelector("body").classList.add(mode);
-}
-
-function light(mode) {
-  document.querySelector("body").classList.remove(mode);
-}
